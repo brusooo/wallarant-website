9c613d47f096e60063bc4ed8acad9dd91a33bc64
brusooo <jenifar200227@gmail.com>
2023-06-11 16:46:48 +0530

    Moved Hangers

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ import Skewdiv from "./components/Skewdiv";
 const App = () => {
   const skewNumbers = [1, 2, 3, 4];
   return (
-    <div className="max-w-full ml-auto mr-auto pl-6 pr-6">
+    <div className="max-w-full ml-auto mr-auto">
       <div className="absolute top-0 z-20">
         <Navbar />
       </div>
 
       {/* hanging piece 1 */}
-      <div className="fixed top-0 sm:left-24 left-12 w-36 h-96 bg-pink-50 rounded-b-full"></div>
+      <div className="fixed top-0 sm:left-24 -left-20 w-36 h-96 bg-pink-50 rounded-b-full"></div>
 
       {/* hanging piece 2 */}
-      <div className="fixed top-0 sm:right-36 right-12 w-36 h-80 bg-pink-50 rounded-b-full"></div>
+      <div className="fixed top-0 sm:right-36 -right-8 w-36 h-80 bg-pink-50 rounded-b-full"></div>
 
       <div className="flex flex-col min-h-screen justify-center align-center py-32 max-sm:py-48">
         {/* legendary text */}
 
-        <div className="flex gap-5 md:text-9xl sm:text-8xl text-7xl z-20 flex-col justify-center items-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-700">
+        <div className="flex gap-5 md:text-9xl sm:text-8xl text-6xl z-20 flex-col justify-center items-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-700">
           <h1 className="font-camar">Legendary</h1>
           <Skewdiv />
           <h1 className="font-camar">For You</h1>
